3c8d60c4ed8e363e5824c214c189f3c76b333b24
MendozaEscamillaCirilo <36929346+MendozaEscamillaCirilo@users.noreply.github.com>
2018-09-26 15:17:36 -0500

    Add files via upload

--- src/uno/Ventana.java
+++ src/uno/Ventana.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uno;
 
-/**
- *
- * @author cirilo
- */
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
@@ -49,3 +41,4 @@ public class Ventana extends JFrame{
         panel.add(chartPanel);
     }
 }
+ 
